bf4d8717e3ef20fb6c8fcef11a0cea6970d6fb02
teilor <teilorsoares@gmail.com>
2023-07-14 01:10:19 -0300

    new view

--- app/src/main/java/com/example/pantrypal4/MainActivity.kt
+++ app/src/main/java/com/example/pantrypal4/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.pantrypal4
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.pantrypal4.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Inicio())
+
+        binding.bottomNavigationView.setOnItemReselectedListener {
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(Inicio())
+                R.id.local -> replaceFragment(Local())
+                R.id.refeicao -> replaceFragment(Refeicao())
+                R.id.lista -> replaceFragment(Lista())
+
+                else ->{
+
+                }
+            }
+            true
+        }
+        val button: FloatingActionButton = findViewById(R.id.floatingActionButton2)
+        button.setOnClickListener {
+            val intent = Intent(this, Novo::class.java)
+            startActivity(intent)
+        }
     }
+
+    private fun replaceFragment(fragment : Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+    }
+
 }
---
+++ app/src/main/java/com/example/pantrypal4/Novo.kt
@@ -0,0 +1,11 @@
+package com.example.pantrypal4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Novo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_novo)
+    }
+}
